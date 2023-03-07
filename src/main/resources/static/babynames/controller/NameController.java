58c8ed69589a3a2733a320a58696cb1024f742cd
rade <rmarinkovic.99@gmail.com>
2023-03-07 20:46:03 +0100

    renamed:    target/classes/BabynamesApplication.java -> src/main/resources/static/babynames/BabynamesApplication.java renamed:    target/classes/controller/NameController.java -> src/main/resources/static/babynames/controller/NameController.java renamed:    target/classes/model/Name.java -> src/main/resources/static/babynames/model/Name.java

--- src/main/resources/static/babynames/controller/NameController.java
+++ src/main/resources/static/babynames/controller/NameController.java
@@ -6,14 +6,18 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
+import org.apache.el.stream.Optional;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.opencsv.CSVReader;
 import ch.zhaw.babynames.model.Name;
 
@@ -22,37 +26,44 @@ public class NameController {
     private ArrayList<Name> listOfNames;
 
     @GetMapping("/names")
-    public List<Name> getNames() {
+    public ArrayList<Name> getNames() {
         return listOfNames;
     }
 
     @GetMapping("/names/count")
     public long getCount(@RequestParam(required = false) String sex) {
+        long count = listOfNames.size();
         if (sex != null) {
-            return listOfNames.stream().filter(x -> x.getGeschlecht().equals(sex)).count();
+            return listOfNames.stream().filter(x -> sex.equalsIgnoreCase(x.getGeschlecht())).count();
         } else {
-            return listOfNames.size();
+            return count;
         }
     }
 
     @GetMapping("/names/frequency")
-    public int getFrequency(@RequestParam String name) {
-        return listOfNames.stream()
-            .filter(x -> x.getName().equalsIgnoreCase(name))
-            .map(x -> x.getAnzahl())
-            .findFirst().orElse(0);
-    }
-
+    public long getFrequency(@RequestParam String name) {
+        Name matchingObject = listOfNames.stream().filter(x -> x.getName().equals(name)).findAny().orElse(null);
+        if (matchingObject == null) {
+            return 0;
+        } else {
+            return matchingObject.getAnzahl();
+        }
+    }  
+    
     @GetMapping("/names/name")
-    public List<String> filterNames(@RequestParam String sex, @RequestParam String start,
+    public ResponseEntity <List<String>> filterNames(@RequestParam String sex, @RequestParam String start,
             @RequestParam int length) {
         List<String> names = listOfNames.stream()
-                .filter(x -> x.getName().startsWith(start))
+                .filter(x -> x.getName().toLowerCase().startsWith(start.toLowerCase()))
                 .filter(x -> x.getName().length() == length)
                 .filter(x -> x.getGeschlecht().equals(sex))
                 .map(x -> x.getName())
                 .collect(Collectors.toList());
-        return names;
+        if ("w".equals(sex) || "m".equals(sex)) {
+            return new ResponseEntity<>(names, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @EventListener(ApplicationReadyEvent.class)
