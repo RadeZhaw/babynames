7bebe86cb906154deb42413e12c1ebfd32b47675
rade <rmarinkovic.99@gmail.com>
2023-03-07 19:39:18 +0100

    implemented status codes.

---
+++ src/main/java/ch/zhaw/babynames/model/Name.java
@@ -0,0 +1,15 @@
+package ch.zhaw.babynames.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+// TODO: Annotationen für Getter, Konstruktor und toString einfügen
+public class Name { 
+ public Name(String string, String string2, int parseInt) {
+    }
+private String name; 
+ private String geschlecht; 
+ private int anzahl; 
+} 
